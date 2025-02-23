9f0765b72f5bdfe193596ef70081663f9885648e
Yahya <yahya6789@gmail.com>
2025-02-23 09:37:12 +0700

    Replace CSV generation with a simple Hello World output.

--- src/main/java/io/github/yahya6789/dataforge/App.java
+++ src/main/java/io/github/yahya6789/dataforge/App.java
@@ -1,10 +1,7 @@
 package io.github.yahya6789.dataforge;
 
-import java.nio.file.Paths;
-
 public class App {
   public static void main(String[] args) {
-    CsvWriter salesCsv = new SalesCsvGenerator(10, Paths.get("sales.csv"));
-    salesCsv.generate();
+    System.out.println("Hello, world!");
   }
 }
