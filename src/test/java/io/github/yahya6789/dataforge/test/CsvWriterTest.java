aa0237eb3b742eb0df3aa3d3f3cfe329ed4ab211
Yahya <yahya6789@gmail.com>
2025-02-24 09:45:06 +0700

    test: tambah shouldDisplayCorrectOutput_whenWritingToSysout
    Menambahkan pengujian baru untuk memastikan output yang benar ditampilkan saat menulis ke System.out.

--- src/test/java/io/github/yahya6789/dataforge/test/CsvWriterTest.java
+++ src/test/java/io/github/yahya6789/dataforge/test/CsvWriterTest.java
@@ -1,15 +1,23 @@
 package io.github.yahya6789.dataforge.test;
 
+import java.io.OutputStreamWriter;
+
 import org.junit.jupiter.api.Test;
 
 import io.github.yahya6789.dataforge.impl.template.CsvTemplate;
 import io.github.yahya6789.dataforge.impl.template.SalesCsvTemplate;
 import io.github.yahya6789.dataforge.impl.writer.Slf4jWriter;
 
-public class Slf4JWriterTest {
+public class CsvWriterTest {
   @Test
-  public void shouldDisplayCorrectOutput_whenWriting() {
+  public void shouldDisplayCorrectOutput_whenWritingToSlf4j() {
     CsvTemplate salesCsv = new SalesCsvTemplate(10);
     salesCsv.generate(new Slf4jWriter());
+  }
+
+  @Test
+  public void shouldDisplayCorrectOutput_whenWritingToSysout() {
+    CsvTemplate salesCsv = new SalesCsvTemplate(10);
+    salesCsv.generate(new OutputStreamWriter(System.out));
   }
 }
